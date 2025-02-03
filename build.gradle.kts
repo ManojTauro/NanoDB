99cdf0a50fea2656b6f8a3371ec77b216c64a0c4
manu2931 <manojb912@gmai.com>
2025-02-03 18:32:05 +0000

    committing IDE and gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "nanodb"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://projectlombok.org/https://projectlombok.org/"
+)
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.projectlombok:lombok:1.18.24")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
